2ac118ca6c4d774629f73755be773480c569f6da
sagrudd <stephen@mnemosyne.co.uk>
2020-12-10 14:09:01 +0000

    first demonstration of minimum functionality for fast5 code

--- R/fast5.R
+++ R/fast5.R
@@ -56,7 +56,7 @@ getH5list <- function(path=NULL, f5content=NULL) {
     if (!is.null(path) & is.null(f5content)) {
         f5content = file.isHdf5(path)
         if (is.logical(f5content)) {
-            return(f5content)
+            return(NULL)
         }
     }
     return(f5content)
@@ -108,6 +108,8 @@ file.isSingleFast5 <- function(path=NULL, f5content=NULL, silent=FALSE) {
 #' @param silent should messages be hidden? (FALSE)
 #' @return boolean
 #'
+#' @seealso \code{\link{file.isSingleFast5} \link{file.isFast5}}
+#'
 #' @importFrom dplyr filter
 #' @importFrom rlang .data
 #' @examples
@@ -125,7 +127,7 @@ file.isMultiFast5 <- function(path=NULL, f5content=NULL, silent=FALSE) {
     }
     itemA = dplyr::filter(f5content, .data$group=="/")[["name"]][1]
     if (all(
-        c("Analyses", "Raw", "channel_id", "context_tags", "tracking_id") %in%
+        c("Raw", "channel_id", "context_tags", "tracking_id") %in%
         dplyr::filter(f5content, .data$group==paste0("/",itemA))[["name"]])) {
         return(TRUE)
     }
@@ -135,6 +137,208 @@ file.isMultiFast5 <- function(path=NULL, f5content=NULL, silent=FALSE) {
 }
 
 
-fast5info <- function(path) {
+#' Extract the sequencing platform identified within a FAST5 file
+#'
+#' This method parses a FAST5 sequence file and extracts the information on
+#' specified sequencing platform - this can be used to identify e.g. gridion or
+#' promethion based information.
+#'
+#' @param path The file.path to check
+#' @param atomic whether a single entity (multi files only) should be considered
+#'     (TRUE)
+#' @return vector of platform(s)
+#'
+#' @examples
+#' promFast5 <- system.file("extdata", "prom.fast5", package="floundeR")
+#' fast5.get_platform(promFast5)
+#'
+#' @export
+fast5.get_platform <- function(path, atomic=TRUE) {
+    anno <- fast5.get_info(path, atomic)
+    return(anno[,"device_type"])
+}
+
+
+#' Extract the flowcell_id identified within a FAST5 file
+#'
+#' This method parses a FAST5 sequence file and extracts the information on
+#' specified flowcell_id - this can be used to track flowcells used within a
+#' laboratory
+#'
+#' @param path The file.path to check
+#' @param atomic whether a single entity (multi files only) should be considered
+#'     (TRUE)
+#' @return vector of flowcell_id(s)
+#'
+#' @examples
+#' multiFast5 <- system.file("extdata", "multi.fast5", package="floundeR")
+#' fast5.get_flowcell_id(multiFast5)
+#'
+#' @export
+fast5.get_flowcell_id <- function(path, atomic=TRUE) {
+    anno <- fast5.get_info(path, atomic)
+    return(anno[,"flow_cell_id"])
+}
+
+
+#' Extract the experiment start time identified within a FAST5 file
+#'
+#' This method parses a FAST5 sequence file and extracts the information on
+#' specified experiment start time - this can be used in palaeogenomics of such
+#' ancient datasets
+#'
+#' @param path The file.path to check
+#' @param atomic whether a single entity (multi files only) should be considered
+#'     (TRUE)
+#' @return vector of experiment_start_time(s)
+#'
+#' @examples
+#' multiFast5 <- system.file("extdata", "multi.fast5", package="floundeR")
+#' fast5.get_exp_start_time(multiFast5)
+#'
+#' @export
+fast5.get_exp_start_time <- function(path, atomic=TRUE) {
+    anno <- fast5.get_info(path, atomic)
+    return(lubridate::ymd_hms(anno$exp_start_time))
+}
+
+
+#' Extract the number of reads identified within a FAST5 file
+#'
+#' This method parses a FAST5 sequence file and extracts the information on
+#' the read count - this is a simple count only
+#'
+#' @param path The file.path to check
+#' @return integer count of reads
+#'
+#' @examples
+#' multiFast5 <- system.file("extdata", "multi.fast5", package="floundeR")
+#' fast5.get_read_count(multiFast5)
+#'
+#' @export
+fast5.get_read_count <- function(path) {
+    f5content = getH5list(path)
+    if (is.null(f5content)) {
+        message("something has gone awry")
+        return(0)
+    }
+    if (file.isSingleFast5(f5content=f5content, silent=TRUE)) {
+        return(1)
+    } else if (file.isMultiFast5(f5content=f5content, silent=TRUE)) {
+        named_sequences = dplyr::filter(f5content, .data$group=="/")[["name"]]
+        return(length(named_sequences))
+    }
+    return(0)
+}
+
+
+
+#' Extract the flowcell type identified within a FAST5 file
+#'
+#' This method parses a FAST5 sequence file and extracts the information on
+#' the flowcell type - this is based on user entry at start of run.
+#'
+#' @param path The file.path to check
+#' @param atomic whether a single entity (multi files only) should be considered
+#'     (TRUE)
+#' @return vector of flowcell_types(s)
+#'
+#' @examples
+#' multiFast5 <- system.file("extdata", "multi.fast5", package="floundeR")
+#' fast5.get_flowcell_type(multiFast5)
+#'
+#' @export
+fast5.get_flowcell_type <- function(path, atomic=TRUE) {
+    anno <- fast5.get_info(path, atomic)
+    return(anno[,"flowcell_type"])
+}
+
+
+#' Extract the sequencing kit identified within a FAST5 file
+#'
+#' This method parses a FAST5 sequence file and extracts the information on
+#' the sequencing_kit used - this is based on user entry at start of run.
+#'
+#' @param path The file.path to check
+#' @param atomic whether a single entity (multi files only) should be considered
+#'     (TRUE)
+#' @return vector of sequencing_kit(s)
+#'
+#' @examples
+#' multiFast5 <- system.file("extdata", "multi.fast5", package="floundeR")
+#' fast5.get_sequencing_kit(multiFast5)
+#'
+#' @export
+fast5.get_sequencing_kit <- function(path, atomic=TRUE) {
+    anno <- fast5.get_info(path, atomic)
+    return(anno[,"sequencing_kit"])
+}
+
+
+
+#' @importFrom rhdf5 h5closeAll
+#' @importFrom rhdf5 h5readAttributes
+fast5.get_attributes <- function(key, path) {
+    tracking_attrs <- rhdf5::h5readAttributes(
+        path, paste0("/", key, "/tracking_id"))
+    rhdf5::h5closeAll()
+    context_attrs <- rhdf5::h5readAttributes(
+        path, paste0("/", key, "/context_tags"))
+    rhdf5::h5closeAll()
+    return(data.frame(
+        device_type=tracking_attrs$device_type,
+        flow_cell_id=tracking_attrs$flow_cell_id,
+        exp_start_time=tracking_attrs$exp_start_time,
+        experiment_type=context_attrs$experiment_type,
+        flowcell_type=context_attrs$flowcell_type,
+        sequencing_kit=context_attrs$sequencing_kit))
+}
+
+
+#' Extract salient experimental information from a FAST5 file
+#'
+#' The FAST5 sequence file contains information relating to the sequencing
+#' platform, flowcell and library preparation kits used in a study. This method
+#' pulls these information into a data.frame or factor.
+#'
+#' @param path The file.path to check
+#' @param atomic whether a single entity (multi files only) should be considered
+#'     (FALSE)
+#' @return data.frame
+#'
+#' @seealso \code{\link{file.isSingleFast5} \link{file.isFast5}}
+#' @importFrom purrr map
+#' @importFrom dplyr filter
+#' @importFrom stats runif
+#'
+#' @examples
+#' promFast5 <- system.file("extdata", "prom.fast5", package="floundeR")
+#' fast5.get_info(promFast5)
+#'
+#' @export
+fast5.get_info <- function(path, atomic=FALSE) {
+    f5content = getH5list(path)
+    if (is.null(f5content)) {
+        message("something has gone awry")
+        return(NULL)
+    }
+    lookup_key = NULL
+    if (file.isSingleFast5(f5content=f5content, silent=TRUE)) {
+        lookup_key = "UniqueGlobalKey"
+    } else if (file.isMultiFast5(f5content=f5content, silent=TRUE)) {
+        lookup_key = dplyr::filter(f5content, .data$group=="/")[["name"]]
+        if (atomic) {
+            pick = floor(runif(1, min=1, max=length(lookup_key)+1))
+            lookup_key = lookup_key[pick]
+        }
+    }
+    if (is.null(lookup_key)) {
+        message(paste0("path [",path,"] provides HDF but not nanopore FAST5"))
+        return(NULL)
+    }
+    l <- purrr::map_df(lookup_key, ~fast5.get_attributes(.x, path=path))
+    return(l)
 
 }
+
+
--- tests/testthat/test-fast5.R
+++ tests/testthat/test-fast5.R
@@ -3,21 +3,60 @@ context("FAST5 file testing")
 singleFast5 <- system.file("extdata", "single.fast5", package="floundeR")
 notFast5 <- system.file("extdata", "not_fast5.fast5", package="floundeR")
 multiFast5 <- system.file("extdata", "multi.fast5", package="floundeR")
+promFast5 <- system.file("extdata", "prom.fast5", package="floundeR")
 
 test_that("single.fast5", {
     expect_equal(file.isFast5(dirname(singleFast5)), FALSE)
     expect_equal(file.isFast5(singleFast5), TRUE)
     expect_equal(file.isSingleFast5(singleFast5), TRUE)
     expect_equal(file.isMultiFast5(singleFast5), FALSE)
+    expect_equal(fast5.get_platform(singleFast5), "gridion")
+    expect_equal(fast5.get_flowcell_id(singleFast5), "FAK42335")
+    expect_equal(fast5.get_exp_start_time(singleFast5),
+                 lubridate::ymd_hms("2019-01-15 15:54:24 UTC"))
+    expect_equal(fast5.get_read_count(singleFast5), 1)
+    expect_equal(fast5.get_sequencing_kit(singleFast5), "sqk-lsk109")
+    expect_equal(fast5.get_flowcell_type(singleFast5), "flo-min106")
 })
 
 test_that("not_fast5.fast5", {
     expect_equal(file.isFast5(notFast5), FALSE)
+    expect_equal(file.isSingleFast5(notFast5), FALSE)
+    expect_equal(file.isSingleFast5(notFast5), FALSE)
+    expect_equal(fast5.get_platform(notFast5), NULL)
+    expect_equal(fast5.get_flowcell_id(notFast5), NULL)
+    # expect_equal(fast5.get_exp_start_time(notFast5), "2019-01-15 15:54:24 UTC")
+    expect_equal(fast5.get_read_count(notFast5), 0)
+    expect_equal(fast5.get_sequencing_kit(notFast5), NULL)
+    expect_equal(fast5.get_flowcell_type(notFast5), NULL)
 })
 
 test_that("multi.fast5", {
     expect_equal(file.isFast5(multiFast5), TRUE)
     expect_equal(file.isSingleFast5(multiFast5), FALSE)
     expect_equal(file.isMultiFast5(multiFast5), TRUE)
+    expect_equal(fast5.get_platform(multiFast5), "gridion")
+    expect_equal(fast5.get_flowcell_id(multiFast5), "FAK42335")
+    expect_equal(fast5.get_exp_start_time(multiFast5),
+                 lubridate::ymd_hms("2019-01-15 15:54:24 UTC"))
+    expect_equal(fast5.get_read_count(multiFast5), 10)
+    expect_equal(fast5.get_sequencing_kit(multiFast5), "sqk-lsk109")
+    expect_equal(fast5.get_flowcell_type(multiFast5), "flo-min106")
+})
+
+test_that("prom.fast5", {
+    expect_equal(file.isFast5(promFast5), TRUE)
+    expect_equal(file.isSingleFast5(promFast5), FALSE)
+    expect_equal(file.isMultiFast5(promFast5), TRUE)
+    expect_equal(fast5.get_platform(promFast5), "promethion")
+    expect_equal(is.data.frame(fast5.get_info(promFast5)), TRUE)
+    expect_equal(nrow(fast5.get_info(promFast5, atomic=TRUE)), 1)
+    expect_equal(nrow(fast5.get_info(promFast5, atomic=FALSE)), 25)
+    expect_equal(fast5.get_flowcell_id(promFast5), "")
+    expect_equal(fast5.get_exp_start_time(promFast5),
+                 lubridate::ymd_hms("2018-05-08 15:53:32 UTC"))
+    expect_equal(fast5.get_read_count(promFast5), 25)
+    expect_equal(fast5.get_sequencing_kit(promFast5), "sqk-lsk109")
+    expect_equal(fast5.get_flowcell_type(promFast5), "flo-pro001")
 })
 
