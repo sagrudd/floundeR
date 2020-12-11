f37fd88bfb3fa8fa8bb234c720e6f79346fc5dad
sagrudd <stephen@mnemosyne.co.uk>
2020-12-11 14:13:46 +0000

    SequencingSummary - parse of provided files

--- R/sequencing_summary.R
+++ R/sequencing_summary.R
@@ -7,33 +7,85 @@
 #' information that can be used to assess the performance of a run.
 #'
 #' @import R6
-
+#' @importFrom utils file_test
 #'
 #' @export
-SequencingSummary <- R6::R6Class("SequencingSummary",
-                     public = list(
-                         #' @field sequencing_summary_file the file.path
-                         #' to the query FAST5 file
-                         sequencing_summary_file = NULL,
-                         #' @field barcoding_summary_file the file.path
-                         #' to the query FAST5 file
-                         barcoding_summary_file = NULL,
+SequencingSummary <- R6::R6Class(
+    "SequencingSummary",
+    public = list(
+        #' @field sequencing_summary_file the file.path
+        #' to the query FAST5 file
+        sequencing_summary_file = NULL,
+        #' @field barcoding_summary_file the file.path
+        #' to the query FAST5 file
+        barcoding_summary_file = NULL,
 
-                         #' @description
-                         #' Creates a new SequencingSummary object. This
-                         #' initialisation method performs other sanity checking
-                         #' of the defined file(s) to ensure that it is indeed
-                         #' parseable and creates the required data structures.
-                         #'
-                         #' @param sequencing_summary_file The source
-                         #' sequencing_summary file.
-                         #' @param barcoding_summary_file The source
-                         #' barcoding_summary_file file.
-                         #' @return A new `Fast5` object.
-                         initialize = function(
-                             sequencing_summary_file = NA,
-                             barcoding_summary_file = NA) {
+        #' @description
+        #' Creates a new SequencingSummary object. This
+        #' initialisation method performs other sanity checking
+        #' of the defined file(s) to ensure that it is indeed
+        #' parseable and creates the required data structures.
+        #'
+        #' @param sequencing_summary_file The source
+        #' sequencing_summary file.
+        #' @param barcoding_summary_file The source
+        #' barcoding_summary_file file.
+        #' @return A new `Fast5` object.
+        #'
+        #' @examples
+        #' sequencing_summary <- system.file("extdata",
+        #'     "sequencing_summary.txt.bz2", package="floundeR")
+        #' barcodes_summary <- system.file("extdata",
+        #'     "barcoding_summary.txt.bz2", package="floundeR")
+        #' seqsum <- SequencingSummary$new(
+        #'     sequencing_summary_file=sequencing_summary,
+        #'     barcoding_summary_file=barcodes_summary)
+        initialize = function(
+            sequencing_summary_file,
+            barcoding_summary_file = NA) {
 
-                         }
-                     )
+            # first pass QC - let's ensure that this is a single FILE
+            if (is.na(sequencing_summary_file)) {
+                stop("Sequencing_summary_file must be defined")
+            } else if (!is.character(sequencing_summary_file) ||
+                length(sequencing_summary_file) != 1) {
+                stop(paste0(
+                    "SequencingSummary requires a single [file.path] as input"))
+            } else if (!file.exists(sequencing_summary_file)) {
+        stop(paste0("path [",sequencing_summary_file,"] does not exist"))
+            } else if (!utils::file_test("-f", sequencing_summary_file)) {
+                stop(paste0("path [",sequencing_summary_file,
+                    "] is a directory - file reqd"))
+            }
+            self$sequencing_summary_file = sequencing_summary_file
+
+            if (!is.na(barcoding_summary_file)) {
+                if (!is.character(barcoding_summary_file) ||
+                    length(barcoding_summary_file) != 1) {
+                    stop(paste0(
+        "barcoding_summary_file requires a single [file.path] as input"))
+                } else if (!file.exists(barcoding_summary_file)) {
+        stop(paste0("path [",barcoding_summary_file,"] does not exist"))
+                } else if (!utils::file_test("-f", barcoding_summary_file)) {
+                    stop(paste0("path [",barcoding_summary_file,
+                                "] is a directory - file reqd"))
+                }
+                self$barcoding_summary_file = barcoding_summary_file
+            }
+            # and import the TSV data (cleanly)
+            if (!private$.parse_seqsum()) {
+                stop("Failed to import the summary files provided ...")
+            }
+        }
+    ),
+
+    private = list(
+
+        seqsum = NULL,
+
+        .parse_seqsum = function() {
+            return(TRUE)
+        }
+
+    )
 )
--- tests/testthat/test-sequencing_summary.R
+++ tests/testthat/test-sequencing_summary.R
@@ -1,3 +1,47 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+context("Sequencing_summary testing")
+test_that("sequencing_summary", {
+
+    sequencing_summary <- system.file("extdata",
+                                      "sequencing_summary.txt.bz2",
+                                      package="floundeR")
+    # fail with a non-string starter of length == 1
+    expect_error(SequencingSummary$new())
+    expect_error(SequencingSummary$new(sequencing_summary_file=123))
+    # check construction with non_existent file
+    expect_error(SequencingSummary$new(sequencing_summary_file="thisfile.txt"))
+    # and check that the specified extant path is actually a file (not dir)
+    expect_error(SequencingSummary$new(sequencing_summary_file=".."))
+    expect_error(SequencingSummary$new(sequencing_summary_file=sequencing_summary), NA)
+})
+
+
+context("Sequencing_summary + barcoding_summary testing")
+test_that("sequencing_summary + barcode", {
+
+    sequencing_summary <- system.file("extdata",
+                                      "sequencing_summary.txt.bz2",
+                                      package="floundeR")
+    barcodes_summary <- system.file("extdata",
+                                    "barcoding_summary.txt.bz2",
+                                    package="floundeR")
+
+    # fail if barcode is provided but not seq_sum
+    expect_error(SequencingSummary$new(barcoding_summary_file=barcodes_summary))
+    expect_error(
+        SequencingSummary$new(
+            sequencing_summary_file=sequencing_summary,
+            barcoding_summary_file=123))
+    expect_error(
+        SequencingSummary$new(
+            sequencing_summary_file=sequencing_summary,
+            barcoding_summary_file="missing.csv"))
+    expect_error(
+        SequencingSummary$new(
+            sequencing_summary_file=sequencing_summary,
+            barcoding_summary_file=".."))
+    expect_error(
+        SequencingSummary$new(
+            sequencing_summary_file=sequencing_summary,
+            barcoding_summary_file=barcodes_summary), NA)
 })
