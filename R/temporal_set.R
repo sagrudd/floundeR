b0dce2b74163e44a0de91672e8dbd7317cf85782
sagrudd <stephen@mnemosyne.co.uk>
2020-12-18 08:08:48 +0000

    added a runtime funtion

--- R/temporal_set.R
+++ R/temporal_set.R
@@ -23,6 +23,15 @@ TemporalSet <- R6::R6Class(
 
         as_tibble = function() {
             return(private$seqsum)
+        },
+
+        runtime = function() {
+            runtime <- max(private$seqsum[,"start_time"]) / 60 / 60
+            expectedRuntimes <- c(4,8,12,24,36,48,64,72,96)
+            temporaldistance <- sqrt((expectedRuntimes - runtime)^2)
+            rruntime <- expectedRuntimes[[which(
+                temporaldistance==min(temporaldistance))]]
+            return(rruntime)
         }
     ),
 
