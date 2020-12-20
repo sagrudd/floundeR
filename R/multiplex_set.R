a834d4ca4f255ee9416ea25b22460a390b4f109c
sagrudd <stephen@mnemosyne.co.uk>
2020-12-20 10:38:22 +0000

    placeholder for the demultiplex content

---
+++ R/multiplex_set.R
@@ -0,0 +1,14 @@
+
+#' R6 Class for loading, visualising and analysing barcode information
+#'
+#' @description
+#'
+#'
+#' @export
+MultiplexSet <- R6::R6Class(
+    inherit = FloundeR,
+    classname = "MultiplexSet",
+    public = list(
+
+    )
+)
