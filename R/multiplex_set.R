4081b27ca9fa1f4a9c81abe7309c6db18a998af3
sagrudd <stephen@mnemosyne.co.uk>
2020-12-21 09:02:15 +0000

    import of barcoding data and properly formed object

--- R/multiplex_set.R
+++ R/multiplex_set.R
@@ -11,38 +11,52 @@ MultiplexSet <- R6::R6Class(
     public = list(
       initialize = function(seqsum=NA, barcoding_summary_file=NA) {
         if (tibble::is_tibble(seqsum)) {
-          if (!"barcode_arrangement" %in% colnames(seqsum) & 
+          if (!"barcode_arrangement" %in% colnames(seqsum) &
               (is.na(barcoding_summary_file) || is.null(barcoding_summary_file))) {
             stop("MultiplexSet requires a `barcode_arrangement` column")
           } else {
-            private$.import_barcoding_summary(seqsum, barcoding_summary_file)
-            stop("MultiplexSet failed trying to import barcoding file")
+            imported <- private$.import_barcoding_summary(
+                seqsum, barcoding_summary_file)
+            print(imported)
+            if (imported < 100) {
+            stop(paste0(
+                "MultiplexSet failed trying to import barcoding file - ",
+                "[",imported,"]% of reads matched seq/barc files"))
+            }
           }
         } else {
           stop("MultiplexSet requires tibble as constructor")
         }
+      },
+
+      #' @description
+      #' Export the imported dataset(s) as a tibble
+      #'
+      #' This object consumes a sequencing summary file (and optionally the
+      #' corresponding barcoding_summary file) and creates an object in
+      #' memory that can be explored, sliced and filtered. This method dumps
+      #' out the in-memory object for further exploration and development.
+      #'
+      #' @return A tibble representation of the starting dataset
+      as_tibble = function() {
+          return(private$seqsum)
       }
-    ), 
-    
+
+    ),
+
     private = list(
       seqsum = NULL,
-      
+
       .import_barcoding_summary = function(seqsum, barcoding_summary_file) {
         barcodedata <- readr::read_tsv(
-          barcoding_summary_file, 
+          barcoding_summary_file,
           col_types = readr::cols_only(
-            "read_id"="c", 
+            "read_id"="c",
             "barcode_arrangement"="c"))
-        
-        pso <- order(seqsum$read_id, method = "radix")
-        seqsum <- seqsum[pso, ]
-        
-        bco <- order(barcodedata$read_id, method = "radix")
-        barcodedata <- barcodedata[bco, ]
-        
-        barcodeMapping <- fmatch(seqsum$read_id, barcodedata$read_id)
-        seqsum$barcode_arrangement <- barcodedata[barcodeMapping,
-                                                  c("barcode_arrangement")]        
+        private$seqsum <- dplyr::inner_join(
+            seqsum, barcodedata, by="read_id") %>%
+            dplyr::select(-read_id)
+        return(nrow(private$seqsum) / nrow(barcodedata) * 100)
       }
     ),
 )
