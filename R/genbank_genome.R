3da8de890f6538dd2a105d9a32332daa4ac9cdd5
sagrudd <stephen@mnemosyne.co.uk>
2021-01-25 12:54:21 +0000

    an example genbank file included (uncompressed)

---
+++ R/genbank_genome.R
@@ -0,0 +1,14 @@
+
+
+#' @export
+GenbankGenome <- R6::R6Class(
+    inherit = FloundeR,
+    classname = "GenbankGenome",
+    public = list(
+
+        initialize = function(gb_file) {
+
+        }
+
+    )
+)
